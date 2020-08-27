491db8b1389ef9e70bbb3163954749e8daf776ec
Bryce Chung <riceman342@gmail.com>
2020-08-26 19:05:19 -0700

    Transition to nodejs from python

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #the next function in code is the function mapped to the route, kinda weird
 @app.route('/',methods=['GET'])
 def loadIndex():
-    return 'index.html'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
